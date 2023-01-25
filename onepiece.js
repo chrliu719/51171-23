d56e1f6759034e2231364014bdf9a358f3ebb411
chrliu719 <liux3770@umn.edu>
2023-01-25 10:06:00 -0600

    Add list feature

--- onepiece.js
+++ onepiece.js
@@ -1,3 +1,13 @@
 function chopper(){
     document.getElementById("tony").style.display='block';
 }
+
+function join(){
+    var name = document.getElementById("name").value;
+    if(name == ""){
+        return;
+    }
+    var node = document.createElement('li');
+    node.appendChild(document.createTextNode(name));
+    document.getElementById("crew").appendChild(node);
+}
